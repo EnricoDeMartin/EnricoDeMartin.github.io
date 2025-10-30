0f9b93552312601f182aae4ca9c1ca4f538acbe1
EnricoDeMartin <demartindiego1@gmail.com>
2025-10-30 17:54:18 +0100

    begin usa17

---
+++ BMW_Oracle/scripts/script.js
@@ -0,0 +1,9 @@
+function spithill(){
+	document.getElementById("blank").src = "images/spithill.jpg";
+}
+function ellison(){
+	document.getElementById("blank").src = "images/ellison.png";
+}
+function blank(){
+	document.getElementById("blank").src = "images/blank.avif";
+}
