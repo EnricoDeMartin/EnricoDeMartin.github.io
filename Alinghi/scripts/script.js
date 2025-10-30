6545160d4dd90811e5ae05fee4f31c9125a025d7
EnricoDeMartin <demartindiego1@gmail.com>
2025-10-30 11:01:14 +0100

    First revision of Home and Alinghi

---
+++ Alinghi/scripts/script.js
@@ -0,0 +1,12 @@
+function bertarelli(){
+	document.getElementById("blank").src = "images/bertarelli.jpg";
+}
+function butterworth(){
+	document.getElementById("blank").src = "images/butterworth.jpg";
+}
+function peyron(){
+	document.getElementById("blank").src = "images/peyron.jpg";
+}
+function blank(){
+	document.getElementById("blank").src = "images/blank.avif";
+}
---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+function upwind(){
+	document.getElementById("courseImage").src = "images/sample.avif";
+}
+function unupwind(){
+	document.getElementById("courseImage").src = "images/Blank_image.jpg";
+}
+function triangle(){
+	document.getElementById("courseImage").src = "images/sample.avif";
+}
+function untriangle(){
+	document.getElementById("courseImage").src = "images/Blank_image.jpg";
+}
