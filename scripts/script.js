0852331e219516830a074b58cad77183cc479e1e
EnricoDeMartin <demartindiego1@gmail.com>
2025-11-05 08:46:05 +0100

    added upwind and triangle course images

--- scripts/script.js
+++ scripts/script.js
@@ -2,11 +2,11 @@ function sample(){
 	document.getElementById("courseImage").src = "images/sample.avif"
 }
 function upwind(){
-	document.getElementById("courseImage").src = "images/sample.avif";
+	document.getElementById("courseImage").src = "images/bastone.jpg";
 }
 function blank(){
 	document.getElementById("courseImage").src = "images/Blank_image.jpg";
 }
 function triangle(){
-	document.getElementById("courseImage").src = "images/sample.avif";
+	document.getElementById("courseImage").src = "images/triangolo.jpg";
 }
